d2ea382ae555709125e85dded907506028cdc89a
chloecharlotte <159797277+chloecharlotte@users.noreply.github.com>
2024-06-04 14:42:29 -0500

    add API functionality

--- src/index.js
+++ src/index.js
@@ -54,6 +54,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "14e2ba01dt6565f0b8o8e18b0aa7193f";
   let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -63,30 +70,30 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let forecastElement = document.querySelector("#forecast");
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="weather-forecast-day">
-      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
       <img
-        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/mist-day.png"
-        width="36"
+        src="${day.condition.icon_url}" class="weather-forecast-icon
       />
+      
       <div class="weather-forecast-temp">
-        <span class="weather-forecast-high">18°</span>
-        <span class="weather-forecast-low">12°</span>
+      <strong>${Math.round(day.temperature.maximum)}°</strong>${Math.round(
+          day.temperature.minimum
+        )}°
       </div>
     </div>
 
 `;
+    }
   });
-
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
